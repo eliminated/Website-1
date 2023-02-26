8fb040dcee384a7d4ab1e5cda652c9eadf3d4462
Golden Smoothy <isaac.nizan@gmail.com>
2023-02-26 18:50:10 +0800

    Minor Update

--- Childs/Videos/videoscript.js
+++ Childs/Videos/videoscript.js
@@ -1,74 +1,41 @@
-// Get elements
-const uploadBtn = document.getElementById("upload-btn");
-const videoInput = document.getElementById("video-input");
-const videoTitleInput = document.getElementById("video-title-input");
-const uploaderInput = document.getElementById("uploader-input");
-const videoContainer = document.getElementById("video-container");
-const deleteBtns = document.querySelectorAll(".delete-btn");
+const form = document.getElementById('upload-form');
+const postBtn = document.getElementById('post-btn');
+const recentPostsList = document.getElementById('recent-posts-list');
 
-// Add event listeners
-uploadBtn.addEventListener("click", function () {
-    videoInput.click();
-});
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-videoInput.addEventListener("change", function () {
-    // Check file type
-    const fileType = videoInput.files[0].type;
-    if (fileType !== "video/mp4") {
-        alert("Please select a video file with .mp4 extension.");
-        return;
-    }
+  const formData = new FormData(event.target);
 
-    // Create video element and add to container
-    const video = document.createElement("video");
-    video.src = URL.createObjectURL(videoInput.files[0]);
-    video.controls = true;
-    video.preload = "metadata";
+  // Create a new video post
+  const videoPost = document.createElement('li');
 
-    // Create title and uploader elements and add to container
-    const title = document.createElement("h2");
-    title.innerText = videoTitleInput.value;
+  // Create a link to the new video post
+  const link = document.createElement('a');
+  link.href = '#'; // Update href to your video page URL
 
-    const uploader = document.createElement("p");
-    uploader.innerText = "Uploaded by: " + uploaderInput.value;
+  // Create the video post title
+  const videoTitle = document.createElement('h3');
+  videoTitle.textContent = formData.get('video-title');
 
-    const infoContainer = document.createElement("div");
-    infoContainer.appendChild(title);
-    infoContainer.appendChild(uploader);
-    infoContainer.classList.add("info-container");
+  // Create the video post uploader
+  const uploader = document.createElement('p');
+  uploader.textContent = `Uploader: ${formData.get('uploader-name')}`;
 
-    // Create delete button and add to container
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-    deleteBtn.classList.add("delete-btn");
+  // Create the video post date
+  const date = new Date();
+  const datePosted = document.createElement('p');
+  datePosted.textContent = `Posted on: ${date.toLocaleString()}`;
 
-    const thumbnailContainer = document.createElement("div");
-    thumbnailContainer.appendChild(video);
-    thumbnailContainer.appendChild(infoContainer);
-    thumbnailContainer.appendChild(deleteBtn);
-    thumbnailContainer.classList.add("thumbnail-container");
+  // Append the video post elements to the link element
+  link.appendChild(videoTitle);
+  link.appendChild(uploader);
+  link.appendChild(datePosted);
 
-    videoContainer.appendChild(thumbnailContainer);
+  // Append the link element to the recent posts list
+  videoPost.appendChild(link);
+  recentPostsList.appendChild(videoPost);
 
-    // Clear input fields
-    videoInput.value = "";
-    videoTitleInput.value = "";
-    uploaderInput.value = "";
-
-    // Add event listener to delete button
-    deleteBtn.addEventListener("click", function () {
-        const confirmDelete = confirm("Are you sure you want to delete this video?");
-        if (confirmDelete) {
-            thumbnailContainer.remove();
-        }
-    });
-
-    // Add hover effect to delete button
-    deleteBtn.addEventListener("mouseover", function () {
-        deleteBtn.style.transform = "scale(1.1)";
-    });
-
-    deleteBtn.addEventListener("mouseout", function () {
-        deleteBtn.style.transform = "scale(1)";
-    });
+  // Clear the form after submission
+  form.reset();
 });
--- script.js
+++ script.js
@@ -10,3 +10,24 @@ toggleButton.addEventListener('click', () => {
     toggleButton.innerHTML = '<img src="./Sources/dark-mode-icon.png" alt="Toggle Dark Mode">';
   }
 });
+
+
+// Context menu
+// Define the function to display the context menu
+function showContextMenu(e) {
+  e.preventDefault();
+  const contextMenu = document.getElementById("myContextMenu");
+  contextMenu.style.display = "block";
+  contextMenu.style.left = e.pageX + "px";
+  contextMenu.style.top = e.pageY + "px";
+}
+
+// Define the function to hide the context menu
+function hideContextMenu() {
+  const contextMenu = document.getElementById("myContextMenu");
+  contextMenu.style.display = "none";
+}
+
+// Attach the event listeners to the document
+document.addEventListener("contextmenu", showContextMenu);
+document.addEventListener("click", hideContextMenu);
